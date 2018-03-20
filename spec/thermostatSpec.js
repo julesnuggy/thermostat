06acd2de69f7212368eaf85906a18c670b970ccf
julesnuggy <julesnuggy@gmail.com>
2018-03-20 14:13:37 +0000

    Initial file set up and first test

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,11 @@
+describe("Thermostat", function() {
+  it("has a starting temperature of 20", function () {
+    thermostat = new Thermostat();
+    expect(thermostat.temp).toEq(20);
+  });
+
+  it("...", function () {
+    ...;
+    expect(...).toEq(...);
+  });
+});
