2aaa2cf73add614a84e85d274fef16bda9229304
julesnuggy <julesnuggy@gmail.com>
2018-03-24 18:13:56 +0000

    Minor change, tidied things up and made sure am on the right commit

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+require 'json'
+
+class ThermostatApp < Sinatra::Base
+  enable :sessions
+
+  get "/" do
+    @temp = session[:temp] || "Current temp: 20C"
+    erb :index
+  end
+
+  get "/temp" do
+    temp = session[:temp]
+
+    content_type :json
+    { temp: temp }.to_json
+  end
+
+  post "/temp" do
+    session[:temp] = params[:temp]
+    #session[:psm] = params[:psm]
+  end
+
+  run! if app_file == $0
+end
---
+++ spec/features/app_spec.rb
@@ -0,0 +1,23 @@
+require 'capybara'
+
+describe ThermostatApp do
+
+  feature 'stores data in /temp when button is pressed' do
+    scenario 'press UP' do
+
+    end
+
+    scenario 'press DOWN' do
+
+    end
+
+    scenario 'press RESET' do
+
+    end
+
+    scenario 'press PSM' do
+
+    end
+
+  end
+end
